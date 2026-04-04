dde1ada7ac857720cb189191cf62be9bd7693ff0
Jordan Greeff <darkroastfitness@gmail.com>
2026-04-04 18:13:54 +0100

    Minor typo fixes and added functionality to automatically add today's date if a user leaves the field blank.

--- src/storage.py
+++ src/storage.py
@@ -1,10 +1,11 @@
 import json
 from pathlib import Path
+from datetime import date
 
 base_dir = Path(__file__).resolve().parent.parent
 file_path_daily = base_dir / "data" / "daily_metrics.json"
 file_path_wkt = base_dir / "data" / "workouts.json"
-#file_path_csv = base_dir / "data" / "wellness_data.csv" -> Used for migrate_csv(). Read more above the function definition (line 34)
+#file_path_csv = base_dir / "data" / "YOURCSVFILEHERE.csv" -> Used for migrate_csv(). Read more above the function definition (line 34)
 
 def create_json():
     file_path_daily.parent.mkdir(parents=True, exist_ok=True)
@@ -13,11 +14,11 @@ def create_json():
 
     if not file_path_daily.exists():
         with open(file_path_daily, 'w') as file:
-            json.dump(default_structure, file, index=2)
+            json.dump(default_structure, file, indent=2)
 
     if not file_path_wkt.exists():
         with open(file_path_wkt, 'w') as file:
-            json.dump(default_structure, file, index=2)
+            json.dump(default_structure, file, indent=2)
 
 def auth_file(file_path):
     if file_path == file_path_daily:
@@ -34,6 +35,18 @@ def auth_file(file_path):
 
     return data, path
 
+def load_data(file_path):
+    create_json()
+    data, _ = auth_file(file_path)
+
+    if not data:
+        print(f"No entries found in {file_path.name}. Initialised empty JSON file.")
+
+    else:
+        print(f"Loaded {len(data)} entries from {file_path.name}")
+
+    return data
+
 def add_entry(file_path):
     data, path = auth_file(file_path)
 
@@ -48,8 +61,12 @@ def add_entry(file_path):
     if path == file_path_daily:
         for key in datapnts_daily:
             value = input(f"Enter {key}:\n")
+
             if key == "Date":
-                datapnts_daily[key] = value
+                if value.strip() == "":
+                    datapnts_daily["Date"] = str(date.today())
+                else:
+                    datapnts_daily["Date"] = value
 
             else:
                 try:
@@ -70,20 +87,7 @@ def add_entry(file_path):
         #add_workout()
         return
 
-
-def load_data(file_path):
-    create_json()
-    data = auth_file(file_path)
-
-    if not data:
-        print(f"No entries found in {file_path.name}. Initialised empty JSON file.")
-
-    else:
-        print(f"Loaded {len(data)} entries from {file_path.name}")
-
-    return data
-
-# Run this function once with an altered .csv file path to migrate your CSV file to JSON format.
+# Run this function once with an altered .csv file path (seen at top of file) to migrate your CSV file to JSON format.
 # def migrate_csv():
 #     with open(file_path_csv, 'r') as file:
 #         reader = csv.DictReader(file, delimiter=',')
@@ -92,3 +96,4 @@ def load_data(file_path):
 
 #         with open(file_path_daily, 'w') as jsonfile:
 #             json.dump(rows, jsonfile)
+# migrate_csv()
